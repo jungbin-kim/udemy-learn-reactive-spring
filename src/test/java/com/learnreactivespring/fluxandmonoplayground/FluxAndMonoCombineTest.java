c4d2a4fa416581e4ee948062fdc699e4f11b1126
Jungbin Kim <mail@jungbin.kim>
2021-05-09 16:14:11 +0900

    26. Virtualizing Time in Junit

--- src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoCombineTest.java
+++ src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoCombineTest.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
 
 public class FluxAndMonoCombineTest {
 
@@ -25,15 +26,24 @@ public class FluxAndMonoCombineTest {
   @Test
   public void combineUsingMerge_withDelay() {
 
+    VirtualTimeScheduler.getOrSet();
+
     Flux<String> flux1 = Flux.just("A", "B", "C").delayElements(Duration.ofSeconds(1));
     Flux<String> flux2 = Flux.just("D", "E", "F").delayElements(Duration.ofSeconds(1));
 
     Flux<String> mergedFlux = Flux.merge(flux1, flux2);
 
-    StepVerifier.create(mergedFlux.log())
+    // Using Virtual time
+    StepVerifier.withVirtualTime(() -> mergedFlux.log())
         .expectSubscription()
+        .thenAwait(Duration.ofSeconds(6))
         .expectNextCount(6)
         .verifyComplete();
+
+//    StepVerifier.create(mergedFlux.log())
+//        .expectSubscription()
+//        .expectNextCount(6)
+//        .verifyComplete();
 
   }
 
