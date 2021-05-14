845873f5f4b2257060088eb3a800d39093587c97
Jungbin Kim <mail@jungbin.kim>
2021-05-15 07:32:58 +0900

    34. Build the simple Non Blocking API using Handler and Router Functions

---
+++ src/main/java/com/learnreactivespring/router/RouterFunctionConfig.java
@@ -0,0 +1,29 @@
+package com.learnreactivespring.router;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+
+import com.learnreactivespring.handler.SampleHandlerFunction;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class RouterFunctionConfig {
+
+  @Bean
+  public RouterFunction<ServerResponse> route(SampleHandlerFunction sampleHandlerFunction) {
+    return RouterFunctions.route(
+        GET("/functional/flux").and(accept(MediaType.APPLICATION_JSON)),
+        sampleHandlerFunction::flux
+    )
+        .andRoute(
+            GET("/functional/mono").and(accept(MediaType.APPLICATION_JSON)),
+            sampleHandlerFunction::mono
+        );
+  }
+
+}
