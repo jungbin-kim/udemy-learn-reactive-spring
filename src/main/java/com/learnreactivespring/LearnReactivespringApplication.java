0a5bf63bff39e01f8c3a627efb4e703feb6d7245
Jungbin Kim <mail@jungbin.kim>
2021-05-15 16:38:38 +0900

    52. Initialize the data using CommandLineRunner

--- src/main/java/com/learnreactivespring/LearnReactivespringApplication.java
+++ src/main/java/com/learnreactivespring/LearnReactivespringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LearnReactivespringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearnReactivespringApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(LearnReactivespringApplication.class, args);
+  }
 
 }
