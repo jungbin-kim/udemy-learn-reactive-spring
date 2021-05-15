b6fbf994f844a74846552b0a03ee2b7dab6397aa
Jungbin Kim <mail@jungbin.kim>
2021-05-15 17:20:54 +0900

    57. Delete an Item Endpoint

--- src/main/java/com/learnreactivespring/controller/v1/ItemController.java
+++ src/main/java/com/learnreactivespring/controller/v1/ItemController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,4 +42,10 @@ public class ItemController {
   public Mono<Item> createItem(@RequestBody Item item) {
     return itemReactiveRepository.save(item);
   }
+
+  @DeleteMapping(ITEM_END_POINT_V1 + "/{id}")
+  public Mono<Void> deleteItem(@PathVariable String id) {
+    return itemReactiveRepository.deleteById(id);
+  }
+
 }
