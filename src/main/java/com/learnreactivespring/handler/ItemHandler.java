53a6ad5cbff46a7db909b225537c49a1d6b43ab6
Jungbin Kim <mail@jungbin.kim>
2021-05-16 12:01:08 +0900

    66. Update an Item - Functional Endpoint

--- src/main/java/com/learnreactivespring/handler/ItemHandler.java
+++ src/main/java/com/learnreactivespring/handler/ItemHandler.java
@@ -50,4 +50,23 @@ public class ItemHandler {
         .contentType(MediaType.APPLICATION_JSON)
         .body(deleteItem, Void.class);
   }
+
+  public Mono<ServerResponse> updateItem(ServerRequest serverRequest) {
+    String id = serverRequest.pathVariable("id");
+    Mono<Item> updatedItem = serverRequest.bodyToMono(Item.class)
+        .flatMap(item -> {
+          Mono<Item> itemMono = itemReactiveRepository.findById(id)
+              .flatMap(currentItem -> {
+                currentItem.setDescription(item.getDescription());
+                currentItem.setPrice(item.getPrice());
+                return itemReactiveRepository.save(currentItem);
+              });
+          return itemMono;
+        });
+    return updatedItem
+        .flatMap(item -> ServerResponse.ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(item))
+        .switchIfEmpty(notFound);
+  }
 }
--- src/main/java/com/learnreactivespring/router/ItemRouter.java
+++ src/main/java/com/learnreactivespring/router/ItemRouter.java
@@ -4,6 +4,7 @@ import static com.learnreactivespring.constants.ItemConstants.ITEM_FUNCTIONAL_EN
 import static org.springframework.web.reactive.function.server.RequestPredicates.DELETE;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.PUT;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
 import com.learnreactivespring.handler.ItemHandler;
@@ -29,7 +30,9 @@ public class ItemRouter {
         .andRoute(POST(ITEM_FUNCTIONAL_END_POINT_V1).and(accept(MediaType.APPLICATION_JSON)),
             itemHandler::createItem)
         .andRoute(DELETE(ITEM_FUNCTIONAL_END_POINT_V1 + "/{id}").and(accept(MediaType.APPLICATION_JSON)),
-            itemHandler::deleteItem);
+            itemHandler::deleteItem)
+        .andRoute(PUT(ITEM_FUNCTIONAL_END_POINT_V1 + "/{id}").and(accept(MediaType.APPLICATION_JSON)),
+            itemHandler::updateItem);
   }
 
 }
