8187e34ed18517d8e5572ad9e4678a95de49ab37
Jungbin Kim <mail@jungbin.kim>
2021-05-16 11:09:40 +0900

    61. Get All Items - Functional Endpoint

--- src/main/java/com/learnreactivespring/router/ItemRouter.java
+++ src/main/java/com/learnreactivespring/router/ItemRouter.java
@@ -1,9 +1,15 @@
 package com.learnreactivespring.router;
 
+import static com.learnreactivespring.constants.ItemConstants.ITEM_FUNCTIONAL_END_POINT_V1;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+
 import com.learnreactivespring.handler.ItemHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Configuration
@@ -11,7 +17,10 @@ public class ItemRouter {
 
   @Bean
   public RouterFunction<ServerResponse> itemsRoute(ItemHandler itemHandler) {
-    return null;
+    return RouterFunctions.route(
+        GET(ITEM_FUNCTIONAL_END_POINT_V1)
+            .and(accept(MediaType.APPLICATION_JSON)),
+        itemHandler::getAllItems);
   }
 
 }
