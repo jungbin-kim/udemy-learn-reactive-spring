336ebd3a5085f96b62bbfc90d2ab0dc0c44999a4
Jungbin Kim <mail@jungbin.kim>
2021-05-16 11:13:05 +0900

    62. Get All Items - JUNIT Test using WebTestClient

---
+++ src/test/java/com/learnreactivespring/handler/ItemHandlerTest.java
@@ -0,0 +1,63 @@
+package com.learnreactivespring.handler;
+
+import com.learnreactivespring.constants.ItemConstants;
+import com.learnreactivespring.document.Item;
+import com.learnreactivespring.repository.ItemReactiveRepository;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@DirtiesContext
+@AutoConfigureWebTestClient
+@ActiveProfiles("test")
+public class ItemHandlerTest {
+
+  @Autowired
+  WebTestClient webTestClient;
+
+  @Autowired
+  ItemReactiveRepository itemReactiveRepository;
+
+  public List<Item> data() {
+    return Arrays.asList(
+        new Item(null, "Samsung TV", 399.99),
+        new Item(null, "LG TV", 329.99),
+        new Item(null, "Apple Watch", 349.99),
+        new Item("ABC", "Beats Headphones", 19.99)
+    );
+  }
+
+  @Before
+  public void setUp() {
+    itemReactiveRepository.deleteAll()
+        .thenMany(Flux.fromIterable(data()))
+        .flatMap(itemReactiveRepository::save)
+        .doOnNext(item -> {
+          System.out.println("Inserted item is : " + item);
+        })
+        .blockLast();
+  }
+
+  @Test
+  public void getAllItems() {
+    webTestClient.get().uri(ItemConstants.ITEM_FUNCTIONAL_END_POINT_V1)
+        .exchange()
+        .expectStatus().isOk()
+        .expectHeader().contentType(MediaType.APPLICATION_JSON)
+        .expectBodyList(Item.class)
+        .hasSize(4);
+  }
+}
