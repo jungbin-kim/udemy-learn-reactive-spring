fc814d0ad89f782e78d741fd622bfa72263da586
Jungbin Kim <mail@jungbin.kim>
2021-05-16 11:48:08 +0900

    과제 2: Write the Junit test case for Create Item Endpoint

--- src/test/java/com/learnreactivespring/handler/ItemHandlerTest.java
+++ src/test/java/com/learnreactivespring/handler/ItemHandlerTest.java
@@ -19,6 +19,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 @SpringBootTest
@@ -108,5 +109,20 @@ public class ItemHandlerTest {
     webTestClient.get().uri(ItemConstants.ITEM_FUNCTIONAL_END_POINT_V1.concat("/{id}"), "DEF")
         .exchange()
         .expectStatus().isNotFound();
+  }
+
+  @Test
+  public void createItem() {
+    Item item = new Item(null, "Iphone X", 999.99);
+
+    webTestClient.post().uri(ItemConstants.ITEM_FUNCTIONAL_END_POINT_V1)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(Mono.just(item), Item.class)
+        .exchange()
+        .expectStatus().isOk()
+        .expectBody()
+        .jsonPath("$.id").isNotEmpty()
+        .jsonPath("$.description").isEqualTo("Iphone X")
+        .jsonPath("$.price").isEqualTo(999.99);
   }
 }
